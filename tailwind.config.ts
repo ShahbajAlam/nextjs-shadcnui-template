ca7d0d0f9a1ff80e1c75bf09c91baa9a459ea2c1
Your Name <you@example.com>
2024-11-16 20:34:16 +0530

    modified:   .gitignore 	modified:   README.md 	deleted:    app/api/auth/[kindeAuth]/route.ts 	new file:   app/fonts/GeistMonoVF.woff 	new file:   app/fonts/GeistVF.woff 	modified:   app/globals.css 	modified:   app/layout.tsx 	modified:   app/page.tsx 	modified:   components.json 	deleted:    components/Navbar.tsx 	deleted:    components/ToggleThemeButton.tsx 	deleted:    components/theme-provider.tsx 	modified:   components/ui/button.tsx 	deleted:    components/ui/dropdown-menu.tsx 	deleted:    database/connectDB.ts 	modified:   lib/utils.ts 	deleted:    next.config.mjs 	new file:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   public/file.svg 	new file:   public/globe.svg 	modified:   public/vercel.svg 	new file:   public/window.svg 	modified:   tailwind.config.ts 	modified:   tsconfig.json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,33 +1,24 @@
 import type { Config } from "tailwindcss";
 
-const config = {
+export default {
     darkMode: ["class"],
     content: [
-        "./pages/**/*.{ts,tsx}",
-        "./components/**/*.{ts,tsx}",
-        "./app/**/*.{ts,tsx}",
-        "./src/**/*.{ts,tsx}",
-    ],
-    prefix: "",
-    theme: {
-        extend: {
-            keyframes: {
-                "accordion-down": {
-                    from: { height: "0" },
-                    to: { height: "var(--radix-accordion-content-height)" },
-                },
-                "accordion-up": {
-                    from: { height: "var(--radix-accordion-content-height)" },
-                    to: { height: "0" },
-                },
-            },
-            animation: {
-                "accordion-down": "accordion-down 0.2s ease-out",
-                "accordion-up": "accordion-up 0.2s ease-out",
-            },
-        },
-    },
-    plugins: [require("tailwindcss-animate")],
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+  	extend: {
+  		colors: {
+  			background: 'var(--background)',
+  			foreground: 'var(--foreground)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
-
-export default config;
