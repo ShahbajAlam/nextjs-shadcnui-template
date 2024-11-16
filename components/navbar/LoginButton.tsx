f2be911eaa306a788af75aac912d0ef0386e70c0
Your Name <you@example.com>
2024-11-16 22:51:23 +0530

    modified:   app/globals.css 	modified:   components/navbar/LoginButton.tsx 	new file:   components/ui/dropdown-menu.tsx 	modified:   package-lock.json 	modified:   package.json

--- components/navbar/LoginButton.tsx
+++ components/navbar/LoginButton.tsx
@@ -1,10 +1,18 @@
+import { LogIn, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import {
     getKindeServerSession,
     LoginLink,
+    LogoutLink,
 } from "@kinde-oss/kinde-auth-nextjs/server";
-import { Button } from "../ui/button";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { LogIn } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default async function LoginButton() {
     const { isAuthenticated, getUser } = getKindeServerSession();
@@ -12,25 +20,49 @@ export default async function LoginButton() {
     const user = await getUser();
 
     return (
-        <div>
+        <>
             {isAuthenticatedUser ? (
-                <Avatar
-                    id="userAvatar"
-                    className="cursor-pointer"
-                >
-                    <AvatarImage
-                        src={user?.picture || ""}
-                        alt={user?.given_name || "User Avatar"}
-                    />
-                    <AvatarFallback>
-                        {user?.given_name ? user.given_name[0] : "U"}
-                    </AvatarFallback>
-                </Avatar>
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar
+                            id="userAvatar"
+                            className="cursor-pointer"
+                        >
+                            <AvatarImage
+                                src={user?.picture || ""}
+                                alt={user?.given_name || "User Avatar"}
+                            />
+                            <AvatarFallback className="animate-pulse">
+                                {user?.given_name ? user.given_name[0] : "U"}
+                            </AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="p-2"
+                        align="end"
+                        sideOffset={10}
+                    >
+                        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem className="cursor-pointer">
+                            Profile
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer">
+                            Billing
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="cursor-pointer">
+                            Team
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="text-red-600">
+                            <LogoutLink>Logout</LogoutLink>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
             ) : (
                 <LoginLink>
                     <LogIn size={30} />
                 </LoginLink>
             )}
-        </div>
+        </>
     );
 }
